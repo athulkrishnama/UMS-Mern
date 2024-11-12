0f834f3ab20c06d7b848408cdecb5ccd2a05ddb6
athulkrishnama <athulkrishnama24@gmail.com>
2024-11-12 14:29:53 +0530

    routes created for home and signup

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,19 @@
-function App() {
-  return (
-    <>
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
-    </>
-  )
+function App() {
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <>home</>,
+    },
+    {
+      path: "/signup",
+      element: <>signup</>,
+    },
+  ]);
+  return (
+      <RouterProvider router={router}></RouterProvider>
+  );
 }
 
-export default App
+export default App;

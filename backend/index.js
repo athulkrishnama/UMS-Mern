06f71c0b726a11df548ee4918a7e89aadbe844e8
athulkrishnama <athulkrishnama24@gmail.com>
2024-11-12 18:43:48 +0530

    mongodb connection established

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,10 @@
 const express = require('express')
 require ('dotenv').config()
 const userRouter = require('./routes/userRoute');
+const mongoose = require('mongoose');
+
+// connnecting with mongodb
+mongoose.connect(process.env.MONGODB_URL).then(()=>console.log('connection successs')).catch((err)=>console.log("Error while connectiing to mongdb"))
 
 const PORT = process.env.PORT ?? 3000;
 const app = express()

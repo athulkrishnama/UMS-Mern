41e6a6e99674665942b65ab7d27ee752f0d33646
athulkrishnama <athulkrishnama24@gmail.com>
2024-11-12 18:37:54 +0530

    route for user signup just created

--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,11 @@
 const express = require('express')
 require ('dotenv').config()
+const userRouter = require('./routes/userRoute');
 
 const PORT = process.env.PORT ?? 3000;
 const app = express()
 
-app.get('/',(req,res)=>{
-    res.json({'sucess':true})
-})
+app.use('/', userRouter)
 
 // listing to port
 app.listen((PORT), () => {

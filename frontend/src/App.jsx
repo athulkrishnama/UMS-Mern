d53014c04acf7adc4b7a9a2d3007cc2f150aeec6
athulkrishnama <athulkrishnama24@gmail.com>
2024-11-13 20:09:50 +0530

    home page protected

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,24 +1,28 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import { Provider} from "react-redux";
+import { Provider } from "react-redux";
 import store from "./store/store";
 import Signup from "./Components/Signup/Signup";
 import Home from "./Components/Home/Home";
-import Login from './Components/Login/Login'
+import Login from "./Components/Login/Login";
+import ProtectedRoute from "./Components/ProtectedRoute/ProtectedRoute";
 function App() {
-  
   const router = createBrowserRouter([
     {
       path: "/",
-      element: <Home/>,
+      element: (
+        <ProtectedRoute>
+          <Home/>
+        </ProtectedRoute>
+      ),
     },
     {
       path: "/signup",
-      element: <Signup/>,
+      element: <Signup />,
     },
     {
-      path:"/login",
-      element: <Login/>
-    }
+      path: "/login",
+      element: <Login />,
+    },
   ]);
   return (
     <Provider store={store}>

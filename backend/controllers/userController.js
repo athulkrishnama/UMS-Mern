0c15ed1b2d8523988a5b6564c467f9ac46c1a76a
athulkrishnama <athulkrishnama24@gmail.com>
2024-11-13 09:45:30 +0530

    create user implemented

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,5 +1,13 @@
+const jwt = require('jsonwebtoken');
 const createUser = (req,res)=>{
-    
+    try{
+        const {username , password, email} = req.body
+        console.log(req.body)
+        const token = jwt.sign({username,password}, process.env.TOKEN_SECRET)
+        res.json({token})
+        }catch(err){
+        console.log(err)
+    }
 }
 
 module.exports = {createUser}

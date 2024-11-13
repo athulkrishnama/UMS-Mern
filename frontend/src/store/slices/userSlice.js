5b51a031de2f3dad3230b003695b136d67f6ba49
athulkrishnama <athulkrishnama24@gmail.com>
2024-11-13 11:19:35 +0530

    minor changes in userSlice

--- frontend/src/store/slices/userSlice.js
+++ frontend/src/store/slices/userSlice.js
@@ -3,11 +3,11 @@ import { createSlice } from "@reduxjs/toolkit";
 const userSlice = createSlice({
     name:"user",
     initialState:{
-        user:{}
+        value:{}
     },
     reducers:{
         setUser:(state,action)=>{
-            state.user = action.payload;
+            state.value = action.payload;
         }
     }
 })

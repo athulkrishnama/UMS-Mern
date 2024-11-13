08efc0f7d2aab9e65567f30095d2685cfee0ec13
athulkrishnama <athulkrishnama24@gmail.com>
2024-11-13 10:01:05 +0530

    signup request sent

--- frontend/src/Components/Signup/Signup.jsx
+++ frontend/src/Components/Signup/Signup.jsx
@@ -1,7 +1,12 @@
 import styles from './Signup.module.css'
+import axios from '../../axios/axios'
 import { useState } from 'react';
+import {useDispatch} from 'react-redux'
+import {setToken} from '../../store/slices/authSlice'
+import {setUser as setUserSlice} from '../../store/slices/userSlice'
 function Signup() {
     const [user, setUser] = useState({username:'',email:'',password:'',image:null});
+    const dispatch = useDispatch()
     const handleDataChange = (e)=>{
         const key = e.target.name;
         if(key === 'image'){
@@ -9,11 +14,22 @@ function Signup() {
         }
         setUser({...user, [key]:e.target.value})
     }
+
+    const handleSubmit = async(e)=>{
+      e.preventDefault();
+      try{
+        const response =await axios.post('/signup', user)
+        dispatch(setToken(response.data.token))
+        dispatch(setUserSlice(response.data.user))
+      }catch(err){
+        console.log(err)
+      }
+    }
   return (
     <div className={styles.signupParent}>
       <div className={styles.signupForm}>
         <h1>Signup</h1>
-        <form>
+        <form onSubmit={handleSubmit}>
             <input type="text" name='username'  onChange={handleDataChange} value={user.username}/>
             <input type="text" name='email'  onChange={handleDataChange} value={user.email}/>
             <input type="password" name='password'  onChange={handleDataChange} value={user.password}/>

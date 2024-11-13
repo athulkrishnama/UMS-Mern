65e3033adf0206952a2e5ef2b7a7889b5c1084b1
athulkrishnama <athulkrishnama24@gmail.com>
2024-11-13 12:55:34 +0530

    hasing password and adding data to database implemented

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,13 +1,30 @@
-const jwt = require('jsonwebtoken');
-const createUser = (req,res)=>{
-    try{
-        const {username , password, email} = req.body
-        console.log(req.body)
-        const token = jwt.sign({username,password}, process.env.TOKEN_SECRET)
-        res.json({token,user:{username,email}})
-        }catch(err){
-        console.log(err)
+const jwt = require("jsonwebtoken");
+const User = require("../model/user");
+const bcrypt = require("bcrypt");
+const createHash = async (password) => {
+  const hashedPassword = await bcrypt.hash(password,10);
+  return hashedPassword;
+};
+const createUser = async (req, res) => {
+  try {
+    const { username, password, email, image } = req.body;
+    const hashedPassword = await createHash(password);
+    const user = new User({
+        username,
+        email,
+        password:hashedPassword,
+        imageUrl:image
+    });
+    const userCredentials = await user.save()
+    console.log(userCredentials)
+    const token = jwt.sign({ username, password }, process.env.TOKEN_SECRET);
+    res.json({ token, user: { username, email } });
+  } catch (err) {
+    if(err.errorResponse.code === 11000){
+       return res.json({err:"already existing credentials"})
     }
-}
+    console.log(err);
+  }
+};
 
-module.exports = {createUser}
+module.exports = { createUser };

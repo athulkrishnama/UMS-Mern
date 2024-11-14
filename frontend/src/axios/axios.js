48af8c663676c4ac0d47610836377f33d9edfd86
athulkrishnama <athulkrishnama24@gmail.com>
2024-11-14 11:56:12 +0530

    intercepter added to axios so auth token will be added in each request

--- frontend/src/axios/axios.js
+++ frontend/src/axios/axios.js
@@ -1,5 +1,18 @@
 import axios from 'axios'
+import store from '../store/store'
 
-export default   axios.create({
+const instance =   axios.create({
     baseURL:'http://localhost:4000/'
 })
+
+instance.interceptors.request.use(config=>{
+    const token = store.getState().auth.token
+    if(token){
+        config.headers['Authorization'] = 'Bearer ' +  token
+    }
+    return config;
+},(err)=>{
+    Promise.reject(err);
+})
+
+export default instance;

59fb62c4334134c1cb48c7300ff55e34d697314a
athulkrishnama <athulkrishnama24@gmail.com>
2024-11-14 12:03:32 +0530

    admin token slice added

--- frontend/src/store/store.js
+++ frontend/src/store/store.js
@@ -1,10 +1,12 @@
 import {configureStore} from '@reduxjs/toolkit'
 import authSlice from './slices/authSlice'
 import userSlice from './slices/userSlice'
+import adminToken from './slices/adminAuthSlice'
 const store = configureStore({
     reducer:{
         auth:authSlice,
-        user:userSlice
+        user:userSlice,
+        adminToken:adminToken
     }
 })
 

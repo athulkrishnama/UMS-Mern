214e0f4390ee737b38420ce4b1e3f6bb378096b3
athulkrishnama <athulkrishnama24@gmail.com>
2024-11-14 11:55:29 +0530

    image upload using cloudinary added to singup page

--- frontend/src/Components/Signup/Signup.jsx
+++ frontend/src/Components/Signup/Signup.jsx
@@ -1,5 +1,6 @@
 import styles from './Signup.module.css'
 import axios from '../../axios/axios'
+import axiosModule from 'axios';
 import { useState } from 'react';
 import {useDispatch} from 'react-redux'
 import {setToken} from '../../store/slices/authSlice'
@@ -7,12 +8,14 @@ import {setUser as setUserSlice} from '../../store/slices/userSlice'
 import { useNavigate } from 'react-router-dom';
 function Signup() {
     const [user, setUser] = useState({username:'',email:'',password:'',image:null});
+    const [image, setImage] = useState(null)
     const [error, setError] = useState('')
     const dispatch = useDispatch()
     const navigate = useNavigate()
     const handleDataChange = (e)=>{
         const key = e.target.name;
         if(key === 'image'){
+            setImage(e.target.files[0])
             return setUser({...user, 'image':URL.createObjectURL(e.target.files[0])})
         }
         setUser({...user, [key]:e.target.value})
@@ -21,9 +24,13 @@ function Signup() {
     const handleSubmit = async(e)=>{
       e.preventDefault();
       try{
-        const response =await axios.post('/signup', user)
+        const formData =new FormData();
+        formData.append('file', image)
+        formData.append('cloud_name','dnkdja8nb')
+        formData.append('upload_preset', 'profilePictures')
+        const imageData = await axiosModule.post('https://api.cloudinary.com/v1_1/dnkdja8nb/image/upload', formData)
+        const response =await axios.post('/signup', {...user, image:imageData.data.secure_url})
         if(response.data.err){
-          console.log(response.data.err)
           return setError(response.data.err)
         }
         dispatch(setToken(response.data.token))

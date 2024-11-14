a6aa19b05878f40ff574e65ee7c1581865bb327e
athulkrishnama <athulkrishnama24@gmail.com>
2024-11-14 18:27:50 +0530

    token checking middleware added to admin

--- backend/routes/adminRoute.js
+++ backend/routes/adminRoute.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const adminController = require('../controllers/adminController');
 const adminRouter = express.Router();
+const auth = require('../middlewares/adminAuth')
 
 adminRouter.post('/login', adminController.login);
+adminRouter.get('/getUsers',auth.tokenChecker,adminController.getUsers)
 
 module.exports = adminRouter;

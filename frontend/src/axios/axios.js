0254b8bdae2f63825da3787d65d454e3910024a0
athulkrishnama <athulkrishnama24@gmail.com>
2024-11-14 18:35:27 +0530

    interceptor added for admin instance to add token in requiest

--- frontend/src/axios/axios.js
+++ frontend/src/axios/axios.js
@@ -18,6 +18,16 @@ instance.interceptors.request.use(config=>{
 const adminInstance = axios.create({
     baseURL:'http://localhost:4000/admin'
 })
+adminInstance.interceptors.request.use(config=>{
+    const token = store.getState().adminToken.token
+    console.log("from interceptor"+token)
+    if(token){
+        config.headers['Authorization'] = 'Bearer ' +  token
+    }
+    return config;
+},(err)=>{
+    Promise.reject(err);
+})
 
 export {adminInstance}
 export default instance;

3cb6e515137038449aab1f8b3aa86ab8a75aeacf
athulkrishnama <athulkrishnama24@gmail.com>
2024-11-14 14:15:17 +0530

    admin login added to route

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,6 +5,7 @@ import Signup from "./Components/Signup/Signup";
 import Home from "./Components/Home/Home";
 import Login from "./Components/Login/Login";
 import ProtectedRoute from "./Components/ProtectedRoute/ProtectedRoute";
+import AdminLogin from "./Components/AdminLogin/AdminLogin";
 function App() {
   const router = createBrowserRouter([
     {
@@ -23,6 +24,10 @@ function App() {
       path: "/login",
       element: <Login />,
     },
+    {
+      path:"/admin/login",
+      element:<AdminLogin/>
+    }
   ]);
   return (
     <Provider store={store}>

72ef9b34bdb2a8d19c97b8eb12e6672061dae6d9
athulkrishnama <athulkrishnama24@gmail.com>
2024-11-14 18:26:37 +0530

    admin home created

---
+++ frontend/src/Components/AdminHome/AdminHome.jsx
@@ -0,0 +1,32 @@
+import React, { useEffect, useState } from "react";
+import { adminInstance } from "../../axios/axios";
+import { useNavigate } from "react-router-dom";
+function AdminHome() {
+    const navigate = useNavigate()
+  const [users, setUsers] = useState([]);
+  useEffect(()=>{
+    adminInstance('/getUsers').then((data)=>{
+        console.log(data)
+        setUsers([...data.data.users])
+    }).catch((err)=>{
+        console.log(err)
+        navigate('/admin/login')
+    })
+  })
+  return (
+    <div>
+      {
+      users.length?<table>
+        {users.map((item,index)=><tr>
+            <td>{item.username}</td>
+            <td>{item.email}</td>
+            <td>edit</td>
+            <td>delete</td>
+        </tr>)}
+      </table>:<h1>NO user found</h1>
+      }
+    </div>
+  );
+}
+
+export default AdminHome;

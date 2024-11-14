0d8452222a9653248bfbe25bc38ff93764693638
athulkrishnama <athulkrishnama24@gmail.com>
2024-11-14 14:14:47 +0530

    userrouter renamed

--- backend/routes/userRoute.js
+++
@@ -1,8 +0,0 @@
-const express =  require('express');
-const userRouter = express.Router();
-const userController = require('../controllers/userController')
-
-userRouter.post('/signup',userController.createUser)
-userRouter.post('/login', userController.login)
-
-module.exports = userRouter;

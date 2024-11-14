fd73c844939dd5aeb69020f44cdff35af3a49c69
athulkrishnama <athulkrishnama24@gmail.com>
2024-11-14 18:27:22 +0530

    get users implemented

--- backend/controllers/adminController.js
+++ backend/controllers/adminController.js
@@ -1,12 +1,24 @@
 const jwt = require('jsonwebtoken')
+const User = require('../model/user')
 const login = (req,res)=>{
     const {email, password} = req.body;
     if(email === process.env.ADMIN_EMAIL && password ===process.env.ADMIN_PASSWORD){
-        const token = jwt.sign({email:process.env.ADMIN_EMAIL},process.env.TOKEN_SECRET)
+        const token = jwt.sign({email:process.env.ADMIN_EMAIL},process.env.ADMIN_TOKEN_SECRET)
         res.json({token, admin:{email}})
     }else{
         res.status(404).json({err:"not authorised"})
     }
 }
 
-module.exports = {login}
+
+const getUsers = async(req,res)=>{
+    try{
+        const users =await User.find();
+        console.log(users)
+        res.status(200).json({users})
+    }catch(error){
+        console.log(error)
+    }
+}
+
+module.exports = {login, getUsers}

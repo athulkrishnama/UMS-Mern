37844d491776a3d7b15f6c0372394f2dc10a2b3a
athulkrishnama <athulkrishnama24@gmail.com>
2024-11-15 14:52:18 +0530

    user home page desing changed

--- frontend/src/Components/Home/Home.jsx
+++ frontend/src/Components/Home/Home.jsx
@@ -1,14 +1,23 @@
-import { useSelector } from "react-redux"
+import { useSelector } from "react-redux";
 function Home() {
-  const user = useSelector(state=>state.user.value)
-  console.log(user)
+  const user = useSelector((state) => state.user.value);
+  console.log(user);
   return (
     <div>
-      <h1>Name: {user.username}</h1>
-      <h2>Email: {user.email}</h2>
-      <img src={user.image} alt="" />
+      <nav>
+        <h2 className="font-sans font-bold text-3xl text-center text-white py-5 bg-purple-700">
+          Welcome {user.username}
+        </h2>
+      </nav>
+      <div className="flex justify-center mt-10">
+        <div className="flex align-middle flex-col">
+          <img className="h-40 w-40 rounded-full" src={user.image} alt="" />
+          <h1 className="mt-3 font-sans  text-2xl">Name: {user.username}</h1>
+          <h2 className="mt-3 font-sans  text-2xl">Email: {user.email}</h2>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Home
+export default Home;

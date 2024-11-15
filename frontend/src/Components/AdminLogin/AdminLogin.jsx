6e2c993678a9a96fe31ec3c5378dcb5631b2ded0
athulkrishnama <athulkrishnama24@gmail.com>
2024-11-15 14:56:56 +0530

    admin login page styled

--- frontend/src/Components/AdminLogin/AdminLogin.jsx
+++ frontend/src/Components/AdminLogin/AdminLogin.jsx
@@ -5,6 +5,7 @@ import { useDispatch } from "react-redux";
 import { useNavigate } from "react-router-dom";
 function AdminLogin() {
     const [user, setUser] = useState({email:'', password:''});
+    const [error, setError] = useState('')
     const dispatch = useDispatch()
     const navigate = useNavigate()
     const handleDataChange = (e)=>{
@@ -18,18 +19,75 @@ function AdminLogin() {
             dispatch(setToken(result.data.token));
             navigate('/admin')
         }catch(err){
+            setError(err.response.data.err)
             console.log(err)
         }
     }
   return (
-    <div>
-        <h1>Admin Login</h1>
-        <form onSubmit={handleSubmit}>
-            <input type="text" name="email" value={user.email} onChange={handleDataChange}/>
-            <input type="password" name="password" value={user.password} onChange={handleDataChange} />
-            <button type="submit">Login</button>
+    <div className="h-[100vh]">
+    <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
+      <div className="sm:mx-auto sm:w-full sm:max-w-sm">
+        <h2 className="mt-10 text-center text-4xl/10 font-bold  tracking-tight text-gray-900">
+         Admin Login
+        </h2>
+      </div>
+      <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+        <form onSubmit={handleSubmit} className="space-y-6">
+          <div>
+            <label
+              htmlFor="email"
+              className="block text-sm/6 font-medium text-gray-900"
+            >
+              Email address
+            </label>
+            <div className="mt-2">
+              <input
+                id="email"
+                name="email"
+                type="email"
+                value={user.email}
+                onChange={handleDataChange}
+                className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm/6"
+              />
+            </div>
+          </div>
+          <div>
+            <div className="flex items-center justify-between">
+              <label
+                htmlFor="password"
+                className="block text-sm/6 font-medium text-gray-900"
+              >
+                Password
+              </label>
+            </div>
+            <div className="mt-2">
+              <input
+                id="password"
+                name="password"
+                type="password"
+                value={user.password}
+                onChange={handleDataChange}
+                className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm/6"
+              />
+            </div>
+          </div>
+          <div className="mt-2">
+            <p className="text-orange-600">{error}</p>
+          </div>
+          <div>
+            <button
+              type="submit"
+              className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm/6 font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+            >
+
+              Sign in
+            </button>
+          </div>
         </form>
+       
+      </div>
     </div>
+  </div>
   )
 }
 

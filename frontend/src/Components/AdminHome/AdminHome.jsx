35d0f4e8be18192af5d347b2f7e833997d7c1924
athulkrishnama <athulkrishnama24@gmail.com>
2024-11-15 17:46:05 +0530

    admin home page design changed

--- frontend/src/Components/AdminHome/AdminHome.jsx
+++ frontend/src/Components/AdminHome/AdminHome.jsx
@@ -7,7 +7,7 @@ function AdminHome() {
   const dispatch = useDispatch();
   const navigate = useNavigate();
   const [users, setUsers] = useState([]);
-  const [search, setSearch] = useState('')
+  const [search, setSearch] = useState("");
   useEffect(() => {
     adminInstance("/getUsers")
       .then((data) => {
@@ -34,34 +34,138 @@ function AdminHome() {
     navigate("/admin/edit");
   };
 
-  const handleSearch = async()=>{
-    try{
-      const response = await adminInstance.post('/search', {search})
-      
-      setUsers(response.data.users)
-    }catch(err){
-      console.log(err)
+  const handleSearch = async () => {
+    try {
+      const response = await adminInstance.post("/search", { search });
+
+      setUsers(response.data.users);
+    } catch (err) {
+      console.log(err);
     }
-  }
+  };
   return (
-    <div>
-      <input type="text" value={search} onChange={(e)=>setSearch(e.target.value)}/>
-      <button onClick={handleSearch}>Search</button>
-      {users.length ? (
-        <table>
-          {users.map((item, index) => (
-            <tr key={index}>
-              <td>{item.username}</td>
-              <td>{item.email}</td>
-              <td onClick={() => editUser(item)}>edit</td>
-              <td onClick={() => deleteUser(item._id)}>delete</td>
-            </tr>
-          ))}
-        </table>
-      ) : (
-        <h1>NO user found</h1>
-      )}
-    </div>
+    <>
+      <nav className="text-center font-sans font-bold text-5xl p-10">Admin Page</nav>
+      <div className="w-full  px-20 bg-white shadow-lg rounded-sm border border-gray-200">
+        <header className="px-5 py-4 border-b border-gray-100">
+          <h2 className="font-semibold text-gray-800">Customers</h2>
+        </header>
+        <div className="px-5">
+          <div className="relative">
+            <input
+              value={search}
+              onChange={(e) => setSearch(e.target.value)}
+              className="w-full bg-transparent placeholder:text-slate-400 text-slate-700 text-sm border border-slate-200 rounded-md pl-3 pr-28 py-2 transition duration-300 ease focus:outline-none focus:border-slate-400 hover:border-slate-300 shadow-sm focus:shadow"
+              placeholder="UI Kits, Dashboards..."
+            />
+            <button
+              onClick={handleSearch}
+              className="absolute top-1 right-1 flex items-center rounded bg-slate-800 py-1 px-2.5 border border-transparent text-center text-sm text-white transition-all shadow-sm hover:shadow focus:bg-slate-700 focus:shadow-none active:bg-slate-700 hover:bg-slate-700 active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
+              type="button"
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 24 24"
+                fill="currentColor"
+                className="w-4 h-4 mr-2"
+              >
+                <path
+                  fillRule="evenodd"
+                  d="M10.5 3.75a6.75 6.75 0 1 0 0 13.5 6.75 6.75 0 0 0 0-13.5ZM2.25 10.5a8.25 8.25 0 1 1 14.59 5.28l4.69 4.69a.75.75 0 1 1-1.06 1.06l-4.69-4.69A8.25 8.25 0 0 1 2.25 10.5Z"
+                  clipRule="evenodd"
+                />
+              </svg>
+              Search
+            </button>
+          </div>
+        </div>
+        <div className="p-3">
+          <div className="overflow-x-auto">
+            {users.length ? (
+              <table className="table-auto w-full">
+                <thead className="text-xs font-semibold uppercase text-gray-400 bg-gray-50">
+                  <tr>
+                    <th className="p-2 whitespace-nowrap">
+                      <div className="font-semibold text-left">Name</div>
+                    </th>
+                    <th className="p-2 whitespace-nowrap">
+                      <div className="font-semibold text-left">Email</div>
+                    </th>
+                    <th className="p-2 whitespace-nowrap">
+                      <div className="font-semibold text-left">Edit</div>
+                    </th>
+                    <th className="p-2 whitespace-nowrap">
+                      <div className="font-semibold text-center">Delete</div>
+                    </th>
+                  </tr>
+                </thead>
+                <tbody className="text-sm divide-y divide-gray-100">
+                  {users.map((user) => (
+                    <tr>
+                      <td className="p-2 whitespace-nowrap">
+                        <div className="flex items-center">
+                          <div className="w-10 h-10 flex-shrink-0 mr-2 sm:mr-3">
+                            <img
+                              className="rounded-full w-10 h-10"
+                              src={user.imageUrl}
+                              width="40"
+                              height="40"
+                              alt="Alex Shatov"
+                            />
+                          </div>
+                          <div className="font-medium text-gray-800">
+                            {user.username}
+                          </div>
+                        </div>
+                      </td>
+                      <td className="p-2 whitespace-nowrap">
+                        <div className="text-left">{user.email}</div>
+                      </td>
+                      <td className="p-2 whitespace-nowrap">
+                        <svg
+                          xmlns="http://www.w3.org/2000/svg"
+                          fill="none"
+                          viewBox="0 0 24 24"
+                          strokeWidth={1.5}
+                          stroke="currentColor"
+                          className="size-6 "
+                          onClick={() => editUser(user)}
+                        >
+                          <path
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
+                            d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L10.582 16.07a4.5 4.5 0 0 1-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 0 1 1.13-1.897l8.932-8.931Zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0 1 15.75 21H5.25A2.25 2.25 0 0 1 3 18.75V8.25A2.25 2.25 0 0 1 5.25 6H10"
+                          />
+                        </svg>
+                      </td>
+                      <td className="p-2 whitespace-nowrap">
+                        <svg
+                          xmlns="http://www.w3.org/2000/svg"
+                          fill="none"
+                          viewBox="0 0 24 24"
+                          strokeWidth={1.5}
+                          stroke="currentColor"
+                          className="size-6 fill-red-600 stroke-white"
+                          onClick={() => deleteUser(user._id)}
+                        >
+                          <path
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
+                            d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0"
+                          />
+                        </svg>
+                      </td>
+                    </tr>
+                  ))}
+                </tbody>
+              </table>
+            ) : (
+              <h1>No user to show</h1>
+            )}
+          </div>
+        </div>
+      </div>
+    </>
   );
 }
 

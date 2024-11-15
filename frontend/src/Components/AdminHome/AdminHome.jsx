4339441d3a929bd1472ba551e5175d0a2797cf5c
athulkrishnama <athulkrishnama24@gmail.com>
2024-11-15 09:53:23 +0530

    search implementd in admin

--- frontend/src/Components/AdminHome/AdminHome.jsx
+++ frontend/src/Components/AdminHome/AdminHome.jsx
@@ -1,30 +1,66 @@
 import React, { useEffect, useState } from "react";
 import { adminInstance } from "../../axios/axios";
 import { useNavigate } from "react-router-dom";
+import { useDispatch } from "react-redux";
+import { setUser } from "../../store/slices/editUserSlice";
 function AdminHome() {
-    const navigate = useNavigate()
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
   const [users, setUsers] = useState([]);
-  useEffect(()=>{
-    adminInstance('/getUsers').then((data)=>{
-        console.log(data)
-        setUsers([...data.data.users])
-    }).catch((err)=>{
-        console.log(err)
-        navigate('/admin/login')
-    })
-  })
+  const [search, setSearch] = useState('')
+  useEffect(() => {
+    adminInstance("/getUsers")
+      .then((data) => {
+        console.log(data);
+        setUsers([...data.data.users]);
+      })
+      .catch((err) => {
+        console.log(err);
+        navigate("/admin/login");
+      });
+  }, []);
+
+  const deleteUser = async (userid) => {
+    try {
+      const response = await adminInstance.post("/deleteUser", { id: userid });
+      setUsers([...response.data.users]);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  const editUser = (user) => {
+    dispatch(setUser(user));
+    navigate("/admin/edit");
+  };
+
+  const handleSearch = async()=>{
+    try{
+      const response = await adminInstance.post('/search', {search})
+      
+      setUsers(response.data.users)
+    }catch(err){
+      console.log(err)
+    }
+  }
   return (
     <div>
-      {
-      users.length?<table>
-        {users.map((item,index)=><tr key={index}>
-            <td>{item.username}</td>
-            <td>{item.email}</td>
-            <td>edit</td>
-            <td>delete</td>
-        </tr>)}
-      </table>:<h1>NO user found</h1>
-      }
+      <input type="text" value={search} onChange={(e)=>setSearch(e.target.value)}/>
+      <button onClick={handleSearch}>Search</button>
+      {users.length ? (
+        <table>
+          {users.map((item, index) => (
+            <tr key={index}>
+              <td>{item.username}</td>
+              <td>{item.email}</td>
+              <td onClick={() => editUser(item)}>edit</td>
+              <td onClick={() => deleteUser(item._id)}>delete</td>
+            </tr>
+          ))}
+        </table>
+      ) : (
+        <h1>NO user found</h1>
+      )}
     </div>
   );
 }

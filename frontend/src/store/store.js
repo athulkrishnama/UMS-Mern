47fbc6bda115138671102a88ed191da6921d19ea
athulkrishnama <athulkrishnama24@gmail.com>
2024-11-15 09:30:25 +0530

    edit user slice added to store

--- frontend/src/store/store.js
+++ frontend/src/store/store.js
@@ -2,11 +2,13 @@ import {configureStore} from '@reduxjs/toolkit'
 import authSlice from './slices/authSlice'
 import userSlice from './slices/userSlice'
 import adminToken from './slices/adminAuthSlice'
+import editUser from './slices/editUserSlice'
 const store = configureStore({
     reducer:{
         auth:authSlice,
         user:userSlice,
-        adminToken:adminToken
+        adminToken:adminToken,
+        editUser
     }
 })
 

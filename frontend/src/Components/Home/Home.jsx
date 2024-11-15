6463836b519ac3cfdded369a07b405ae895a2311
athulkrishnama <athulkrishnama24@gmail.com>
2024-11-15 17:46:31 +0530

    minor change in home page

--- frontend/src/Components/Home/Home.jsx
+++ frontend/src/Components/Home/Home.jsx
@@ -10,7 +10,7 @@ function Home() {
         </h2>
       </nav>
       <div className="flex justify-center mt-10">
-        <div className="flex align-middle flex-col">
+        <div className="flex items-center flex-col">
           <img className="h-40 w-40 rounded-full" src={user.image} alt="" />
           <h1 className="mt-3 font-sans  text-2xl">Name: {user.username}</h1>
           <h2 className="mt-3 font-sans  text-2xl">Email: {user.email}</h2>

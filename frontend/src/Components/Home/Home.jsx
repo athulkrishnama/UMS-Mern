016fad08b72a10b776a2d2e071663e14a8da26c2
athulkrishnama <athulkrishnama24@gmail.com>
2024-11-21 18:50:18 +0530

    minor change in home page

--- frontend/src/Components/Home/Home.jsx
+++ frontend/src/Components/Home/Home.jsx
@@ -1,13 +1,21 @@
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
+import {setToken} from '../../store/slices/authSlice'
+import {useNavigate} from 'react-router-dom'
 function Home() {
   const user = useSelector((state) => state.user.value);
-  console.log(user);
+  const dispatch = useDispatch()
+  const navigate = useNavigate()
+  const handleLogout = ()=>{
+    dispatch(setToken(null))
+    navigate('/login')
+  }
   return (
     <div>
-      <nav>
-        <h2 className="font-sans font-bold text-3xl text-center text-white py-5 bg-purple-700">
+      <nav className="py-5 bg-purple-700 flex justify-between px-20">
+        <h2 className="font-sans font-bold text-3xl  text-white ">
           Welcome {user.username}
         </h2>
+        <button className="border-white  rounded-lg bg-red-600 px-3 text-white font-bold" onClick={handleLogout}>Log Out</button>
       </nav>
       <div className="flex justify-center mt-10">
         <div className="flex items-center flex-col">

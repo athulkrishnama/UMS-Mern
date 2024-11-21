3e594b37c5069074a6bf8fbe23d3a05f12ceeb71
athulkrishnama <athulkrishnama24@gmail.com>
2024-11-21 18:50:45 +0530

    user list added to store

--- frontend/src/store/store.js
+++ frontend/src/store/store.js
@@ -3,12 +3,14 @@ import authSlice from './slices/authSlice'
 import userSlice from './slices/userSlice'
 import adminToken from './slices/adminAuthSlice'
 import editUser from './slices/editUserSlice'
+import  userList from './slices/userList'
 const store = configureStore({
     reducer:{
         auth:authSlice,
         user:userSlice,
         adminToken:adminToken,
-        editUser
+        editUser,
+        userList
     }
 })
 
